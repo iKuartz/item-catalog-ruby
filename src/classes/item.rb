499b8c631f2c1ceff31257678513468f46a0b3dd
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-24 20:26:19 +0500

    Add move_to_archive method

--- src/classes/item.rb
+++ src/classes/item.rb
@@ -1,10 +1,16 @@
 require 'date'
 
 class Item
+  attr_reader :label
 
   def initialize(publish_date)
     @id = Random.rand
     @publish_date = Date.parse(publish_date)
     @archived = false
   end
+
+  def move_to_archive
+    @archive = true if can_be_archived? == true
+  end
+
 end
