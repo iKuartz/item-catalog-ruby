f078940c5a106573e10a50f5ec0c076c4e04ede3
Ivan Silva <ivankuartz@hotmail.com>
2022-05-24 15:47:26 -0300

    Added Type collection class to hold array of objects of a type of item.

---
+++ src/classes/authors.rb
@@ -0,0 +1,3 @@
+require_relative './author'
+require_relative './item_collection'
+require_relative '../modules/json_handler'
---
+++ src/classes/item_collection.rb
@@ -0,0 +1,16 @@
+class TypeCollection
+  attr_reader :list
+
+  def initialize
+    @list = []
+  end
+
+  def get
+    type = @list[0].class.name
+    message = "Select [1] for a #{type} from the list or Select [2] to create a new #{type}: "
+  end
+
+  def display
+    puts @list
+  end
+end
