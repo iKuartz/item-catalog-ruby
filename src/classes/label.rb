45203c5183b8ee58205f6c96b6c18c9a3bbd161f
awaisanwar544 <awaisanwar544@gmail.com>
2022-05-24 20:40:18 +0500

    Add add_item method

--- src/classes/label.rb
+++ src/classes/label.rb
@@ -1,10 +1,14 @@
 class Label
   attr_reader :title, :color
 
-  def initialize
+  def initialize(title, color)
     @id = Random.rand
     @title = title
     @color = color
     @items = []
+  end
+
+  def add_item(item)
+    @items.push(item)
   end
 end
