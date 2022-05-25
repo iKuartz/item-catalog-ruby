6bf55179f576a2d8fb85964e1fa6b51af606bf79
Ivan Silva <ivankuartz@hotmail.com>
2022-05-25 12:05:36 -0300

    Added games collection class and handlers

--- src/classes/item_collection.rb
+++ src/classes/item_collection.rb
@@ -7,7 +7,7 @@ class TypeCollection
 
   def get
     type = @list[0].class.name
-    message = "Select [1] for a #{type} from the list or Select [2] to create a new #{type}: "
+    message = "Select [1] for a #{type} from the list or select [2] to create a new #{type}: "
   end
 
   def display
