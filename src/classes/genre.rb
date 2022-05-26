eed2c5ef59eabac98b31b010e5bcaf408e7c878a
Ntare Guy <gntare2@gmail.com>
2022-05-26 23:58:34 +0200

    Genre methods and collection

--- src/classes/genre.rb
+++ src/classes/genre.rb
@@ -1,15 +1,23 @@
+require_relative '../modules/json_methods'
+
 class Genre
-    attr_reader :name
-  
-    def initialize(id, name)
-      @id = id
-      @name = name
-      @items = []
-    end
-  
-    def add_item(item)
-      item.genre = self
-      @items << item unless @items.include?(item)
-      @items
-    end
+  attr_reader :name
+
+  include JsonMethods
+
+  def initialize(id, name)
+    @id = id
+    @name = name
+    @items = []
+    @args = [id, name]
   end
+
+  def add_item(item)
+    item.genre = self unless item.genre == self
+    @items << item unless @items.include?(item)
+  end
+
+  def self.json_create(object)
+    new(*object['arguments'])
+  end
+end
